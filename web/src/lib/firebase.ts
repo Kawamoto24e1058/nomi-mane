6e57524e2b20d075fb8d1bcc784381ed9d93cea2
haru2514 <kharu0414@icloud.com>
2026-03-16 19:45:58 +0900

    API変更

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -10,16 +10,25 @@ import { browser } from "$app/environment";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { 
+  PUBLIC_FIREBASE_API_KEY,
+  PUBLIC_FIREBASE_AUTH_DOMAIN,
+  PUBLIC_FIREBASE_PROJECT_ID,
+  PUBLIC_FIREBASE_STORAGE_BUCKET,
+  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  PUBLIC_FIREBASE_APP_ID,
+  PUBLIC_FIREBASE_MEASUREMENT_ID
+} from "$env/static/public";
+
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDq-kJH6Z0doodQ2v-ixS1PKkqlW6AOwHI",
-  authDomain: "nomimane.firebaseapp.com",
-  projectId: "nomimane",
-  storageBucket: "nomimane.firebasestorage.app",
-  messagingSenderId: "1035978576239",
-  appId: "1:1035978576239:web:1ac6ab40bb13aab10c189a",
-  measurementId: "G-M3250XNQT4"
+  apiKey: PUBLIC_FIREBASE_API_KEY,
+  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: PUBLIC_FIREBASE_APP_ID,
+  measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
